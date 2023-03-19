577b7a75265b3606f18ac8a2f5070dd342b6c7b5
Martin Luna <uxmoon@gmail.com.com>
2023-03-19 20:01:59 -0300

    Add local state to component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,36 +1,31 @@
-import { MouseEvent } from 'react';
+import { useState } from 'react'
 function ListGroup() {
-  let items = ['Los Angeles', 'Seoul', 'Tokyo', 'Cologne', 'Buenos Aires'];
-  // items = [];
-  // const getMessage = () => {
-  //   return items.length === 0 ? <p>No cities found.</p> : null;
-  // };
-
-  // if (items.length === 0)
-  //   return (
-  //     <>
-  //       <h1>Group List</h1>
-  //       <p>No items found</p>
-  //     </>
-  //   );
-
-  // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event.target);
+  let items = ['Los Angeles', 'Seoul', 'Tokyo', 'Cologne', 'Buenos Aires']
+  const [selectedIndex, setSelectedIndex] = useState(-1)
 
   return (
     <>
       <h1>List</h1>
-      {/* {items.length === 0 ? <p>No cities found.</p> : null} */}
       {items.length === 0 && <p>No cities found.</p>}
-      <ul className="list-group">
+      <ul className='list-group'>
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index)
+            }}
+          >
             {item}
           </li>
         ))}
       </ul>
     </>
-  );
+  )
 }
 
-export default ListGroup;
+export default ListGroup
