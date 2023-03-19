d6f3066a57f5a9b67f23231ceef1a8c73b23be55
Martin Luna <uxmoon@gmail.com.com>
2023-03-19 19:33:46 -0300

    Add first component test

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing, capitalize first letter of each word
+function Message() {
+  // JSX, JavaScript XML
+  const name = 'Moon'
+  if (name)
+    return <h1>Hello {name}</h1>
+  return <h1>Hello user!</h1>
+}
+
+export default Message;
