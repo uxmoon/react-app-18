1a418887fb7012dc7fb7eb947046fe41847d95b5
Martin Luna <uxmoon@gmail.com.com>
2023-03-19 21:12:35 -0300

    Add button and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import ListGroup from './components/ListGroup'
-import Alert from './components/Alert'
+import { Alert } from './components/Alert'
+import { Button } from './components/Button'
 function App() {
   let items = ['Los Angeles', 'Seoul', 'Tokyo', 'Cologne', 'Buenos Aires']
 
@@ -12,6 +13,9 @@ function App() {
       <Alert>
         This is a <em>children text</em>.
       </Alert>
+      <Button color='dark' onClick={() => console.log('click')}>
+        Sample button
+      </Button>
       <ListGroup
         items={items}
         heading='Cities'
