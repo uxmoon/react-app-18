0d2dedcfff1168510db8537979f1eb36f7fa6e10
Martin Luna <uxmoon@gmail.com.com>
2023-03-19 21:11:25 -0300

    Change to arrow function component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,12 +3,11 @@ import { ReactNode } from 'react'
 interface Props {
   children: ReactNode
 }
-function Alert({ children }: Props) {
+
+export const Alert = ({ children }: Props) => {
   return (
     <div className='alert alert-primary' role='alert'>
       {children}
     </div>
   )
 }
-
-export default Alert
